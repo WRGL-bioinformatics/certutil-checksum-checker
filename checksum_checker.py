c321c39e76349cde5b3fab6a12bff49ed111b086
Ben Sanders <ben@13en.co.uk>
2021-08-04 13:30:08 +0100

    Testing gitlab CI

--- checksum_checker.py
+++ checksum_checker.py
@@ -1,6 +1,6 @@
-import sys
-from app.functions.read_checksum_file import ChecksumFile
 from app.interface.interface import ChecksumChecker
 
 if __name__ == "__main__":
     form = ChecksumChecker()
+
+# sThis adds a flake8 error
